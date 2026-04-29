d8fb1019ce658b75ec3298ee4d4dceb1863348dc
MarcoMendoza7 <mendozamarcope29@gmail.com>
2026-04-28 22:23:26 -0600

    Nginx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_BASE = "http://localhost:3000/api";
+const API_BASE = "/api";
 
 function App() {
   const [libros, setLibros] = useState([]);
